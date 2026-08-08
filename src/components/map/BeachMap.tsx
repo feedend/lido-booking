d034f1a256fc7e1f600e51fdf13ceaa7ed979078
feedend <questforfeed@gmail.com>
2026-08-08 17:25:59 +0200

    Clear center values in BeachMap rows configuration

--- src/components/map/BeachMap.tsx
+++ src/components/map/BeachMap.tsx
@@ -195,16 +195,16 @@ export default function BeachMap({ selectedDate, userData }: BeachMapProps) {
   // Configurazione strutturale delle file
   const rows = [
     { startL: 1, endL: 10, startR: 11, endR: 20, center: "Bagnino" },
-    { startL: 21, endL: 30, startR: 31, endR: 40, center: "P" },
-    { startL: 41, endL: 50, startR: 51, endR: 60, center: "A" },
-    { startL: 61, endL: 70, startR: 71, endR: 80, center: "S" },
-    { startL: 81, endL: 90, startR: 91, endR: 100, center: "S" },
-    { startL: 101, endL: 110, startR: 111, endR: 120, center: "E" },
-    { startL: 121, endL: 129, startR: 130, endR: 139, center: "R" },
-    { startL: 140, endL: 146, startR: 147, endR: 154, center: "E" },
-    { startL: 155, endL: 160, startR: 161, endR: 167, center: "L" },
-    { startL: null, endL: null, startR: 168, endR: 171, center: "L" }, // Fila 168-171
-    { startL: null, endL: null, startR: 172, endR: 174, center: "A" }, // Fila 172-174
+    { startL: 21, endL: 30, startR: 31, endR: 40, center: " " },
+    { startL: 41, endL: 50, startR: 51, endR: 60, center: " " },
+    { startL: 61, endL: 70, startR: 71, endR: 80, center: " " },
+    { startL: 81, endL: 90, startR: 91, endR: 100, center: " " },
+    { startL: 101, endL: 110, startR: 111, endR: 120, center: " " },
+    { startL: 121, endL: 129, startR: 130, endR: 139, center: " " },
+    { startL: 140, endL: 146, startR: 147, endR: 154, center: " " },
+    { startL: 155, endL: 160, startR: 161, endR: 167, center: " " },
+    { startL: null, endL: null, startR: 168, endR: 171, center: " " }, // Fila 168-171
+    { startL: null, endL: null, startR: 172, endR: 174, center: " " }, // Fila 172-174
   ];
 
   const renderSpot = (num: number) => {
